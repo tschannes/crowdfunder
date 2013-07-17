d81dd210d0d85b65f31dcc17e21bd6ea57e4fbed
tschannes <mail@tscherrig.net>
2013-07-17 14:34:38 -0400

    fixtures deleted

--- crowdfunder/config/application.rb
+++ crowdfunder/config/application.rb
@@ -1,3 +1,5 @@
+
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -19,5 +21,10 @@ module Crowdfunder
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+      config.generators do |g|
+      g.test_framework  :test_unit, :fixture => false
+    end
+  
   end
 end
