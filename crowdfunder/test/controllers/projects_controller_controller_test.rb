8caea6b40fb23dc56ac16a60cf9f952e0158e31f
tschannes <mail@tscherrig.net>
2013-07-17 20:31:39 -0400

    first test passes

---
+++ crowdfunder/test/controllers/projects_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProjectsControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ crowdfunder/test/helpers/projects_controller_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ProjectsControllerHelperTest < ActionView::TestCase
+end
