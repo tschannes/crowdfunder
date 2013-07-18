06c48d435ea73dda41f87f6723bd5f6aa1b058db
tschannes <mail@tscherrig.net>
2013-07-18 13:03:01 -0400

    forms added

---
+++ crowdfunder/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+		if @user.save
+			flash[:notice] = "Account created"
+			redirect_to root_path
+		else
+			render "new", notice: "Something went wrong."
+		end
+	end
+
+
+private
+    def user_params
+      params.require(:user).permit(:email, :first_name, :last_name)
+    end
+
+end
---
+++ crowdfunder/db/migrate/20130718165015_add_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+  	
+  end
+end
