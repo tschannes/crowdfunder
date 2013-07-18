62bbac45349cae490c54c3a303f5a3ae95b09045
tschannes <mail@tscherrig.net>
2013-07-18 19:33:22 -0400

    failing pledges

--- crowdfunder/test/models/pledge_test.rb
+++ crowdfunder/test/models/pledge_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class PledgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # VALIDATIONS
+  test "requires a user" do 
+    pledge = FactoryGirl.build(:pledge, user: nil)
+    
+    # Should not be able to save
+    refute pledge.save
+    assert_equal "can't be blank", pledge.errors[:user].first
+  end
+  
+  test "requires a project" do 
+    pledge = FactoryGirl.build(:pledge, project: nil)
+
+    # Should not be able to save
+    refute pledge.save
+    assert_equal "can't be blank", pledge.errors[:project].first
+  end
+
+  test "requires amount" do 
+    pledge = FactoryGirl.build(:pledge, amount: nil)
+
+    # Should not be able to save
+    refute pledge.save
+    assert_equal "is not a number", pledge.errors[:amount].first
+  end
+
+  test "requires amount greater than 0" do 
+    pledge = FactoryGirl.build(:pledge, amount: -10)
+
+    # Should not be able to save
+    refute pledge.save
+    assert_equal "must be greater than 0", pledge.errors[:amount].first
+  end
+  
+  test "requires amount to be an integer" do 
+    pledge = FactoryGirl.build(:pledge, amount: 'abc')
+
+    # Should not be able to save
+    refute pledge.save
+    assert_equal "is not a number", pledge.errors[:amount].first
+  end
 end
