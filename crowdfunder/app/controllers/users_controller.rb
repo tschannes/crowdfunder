6f1696d8733766ebd32cf125c0a42e7be3c9bc83
tschannes <mail@tscherrig.net>
2013-07-18 13:08:55 -0400

    flash and error added

--- crowdfunder/app/controllers/users_controller.rb
+++ crowdfunder/app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
 private
     def user_params
-      params.require(:user).permit(:email, :first_name, :last_name)
+      params.require(:user).permit(:email, :first_name, :last_name, :password)
     end
 
 end
